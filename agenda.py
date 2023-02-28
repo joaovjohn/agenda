442aa67599f58fccae22eb9ef4981b2d6cea969e
joaovjohn <joaovjohn@gmail.com>
2023-02-28 13:59:47 -0300

    projeto final

--- agenda.py
+++ agenda.py
@@ -1,6 +1,10 @@
+import os  # utilizado para "limpar" o terminal, deixar a agenda mais limpa
+import time  # utilizado para timer das mensagens, deixar a agenda mais limpa
+
 agenda = []  # lista criada para armazenar os compromissos/informações
-# -------------------------------------------------------
+
 # FUNÇÕES
+
 
 def incluir_compromisso(data, hora, duracao, descricao):
     compromisso = {"data": data, "hora": hora,  # dicionário para cada categoria
@@ -8,7 +12,11 @@ def incluir_compromisso(data, hora, duracao, descricao):
     # adiciona as informações que serão descritas para a agenda
     agenda.append(compromisso)
     print("Compromisso adicionado com sucesso!")
+    time.sleep(3)
+    print("", end='')  # volta para o menu depois de 3 segundos
 
+
+# recebe a data e hora como argumentos e retorna a lista com as informações descritas
 def consultar_compromisso(data, hora=None):
     compromissos = []  # *essa lista funciona somente nessa função*
     for c in agenda:  # percorre a agenda e armazena cada compromisso em "c"
@@ -18,6 +26,8 @@ def consultar_compromisso(data, hora=None):
             compromissos.append(c)
     if not compromissos:  # se "c" não for encontrado, exibe a mensagem abaixo
         print("Agenda vazia")
+        time.sleep(3)
+        print("", end='')  # volta para o menu depois de 3 segundos
         return
     else:
         for c in compromissos:  # se a lista compromissos não estiver vazia, então tem todos os compromissos que satisfazem a busca
@@ -29,7 +39,10 @@ def consultar_compromisso(data, hora=None):
             sair = input("\nDigite 0 para voltar ao menu: ")
             if sair == '0':  # volta para o menu
                 return
+
 # data e hora são obrigatórios - duração e descrição são opcionais
+
+
 def alterar_compromisso(data, hora, duracao=None, descricao=None):
     for c in agenda:  # percorre a agenda e armazena cada compromisso em "c"
         # verifica se a data/hora da agenda correspondem aos que serão informados
@@ -41,9 +54,14 @@ def alterar_compromisso(data, hora, duracao=None, descricao=None):
                 # se a descrição for informada, atualiza com a informação que foi digitada
                 c["descricao"] = descricao
             print("Compromisso alterado com sucesso!")
+            time.sleep(3)
+            print("", end='')  # volta para o menu depois de 3 segundos
             return  # encerra a função
+
     # se a função chegar até aqui, significa que não foi encontrado nenhuma informação com o que foi descrito
     print("Compromisso não encontrado")
+    time.sleep(3)
+    print("", end='')
 
 
 def excluir_compromisso(data, hora):
@@ -51,16 +69,39 @@ def excluir_compromisso(data, hora):
     for i, c in enumerate(agenda):
         # verifica se as informações descritas pelo usuário estão em "c"
         if c["data"] == data and c["hora"] == hora:
-            del agenda[i]  # caso for True, exclui as informações da lista
+            del agenda[i]  # caso True, exclui as informações da lista
             print("Compromisso excluído com sucesso!")
+            time.sleep(3)
+            # apaga a linha anterior onde a mensagem foi exibida
+            print("", end='')
             return  # encerra a função
     # caso a função não entrar no if, exibe essa mensagem na tela
     print("Compromisso não encontrado")
+    time.sleep(3)
+    print("", end='')  # volta para o menu depois de 3 segundos
+
+
+def listar_todos_compromissos():
+    if not agenda:  # se não tiver nenhuma informação na lista, exibe a mensagem
+        print("Agenda vazia")
+        time.sleep(3)
+        print("", end='')  # volta para o menu depois de 3 segundos
+    else:
+        for c in agenda:  # percorre "c" na lista exibindo a data, hora, duração e descrição
+            print("Data:", c["data"])
+            print("Hora:", c["hora"])
+            print("Duração:", c["duracao"])
+            print("Descrição:", c["descricao"])
+        sair = input("\nDigite 0 para voltar ao menu: ")
+        if sair == '0':
+            return
+
 
 # -------------------------------------------------------
 # PROGRAMA
-
 while True:
+    os.system('cls')  # limpa o que está descrito anteriormente
+    print("------------------\n AGENDA \n------------------ ")
     print("1. Incluir compromisso")
     print("2. Consultar compromisso")
     print("3. Alterar compromisso")
@@ -68,6 +109,8 @@ while True:
     print("5. Listar todos os compromissos")
     print("6. Sair")
     opcao = input("Escolha uma opção: ")
+    # "limpa" o terminal para realizar uma das opções escolhidas
+    os.system('cls')
 
     if opcao == "1":
         data = input("Digite a data (dd/mm/aaaa): ")
@@ -92,7 +135,8 @@ while True:
         data = input("Digite a data (dd/mm/aaaa): ")
         hora = input("Digite a hora (hh:mm): ")
         excluir_compromisso(data, hora)
-    # elif opcao == "5":
+    elif opcao == "5":
+        listar_todos_compromissos()
     elif opcao == "6":
         break
     else:
